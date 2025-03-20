61bb397657586e77ac9c111be0af3ff784a41361
SSun <90237785+SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSun@users.noreply.github.com>
2025-03-20 15:28:23 +0900

    3/18

--- main.cpp
+++ main.cpp
@@ -1,27 +1,32 @@
 //------------------------------------------------------------------
-// 2025 STL 화56목78	3월 6일 목요일					(1주 2일)
+// 2025 STL 화56목78	3월 18일 화요일					(3주 1일)
 //------------------------------------------------------------------
-// 많은 수의 자료를 처리하기
+// 많은 수의 자료를 처리하기 - Dog를 읽고 쓴다, read/write
 //------------------------------------------------------------------
 // VS 버전 - 17.13 이상
 // Release / x64, C++언어표준 - /std::c++latest, SDL 검사 - 아니오
 #include <iostream>
-#include <random>
-#include <print>
+#include <string>
 #include "save.h"
 // using namespace std;			// 우리는 이렇게 하면 안된다
-// 
-// [문제] 랜덤 int 1000개를 메모리에 저장하라
-// 가장 큰 값을 찾아 화면에 출력하라
 
-std::default_random_engine dre;
-std::uniform_int_distribution uid{ 0,9999999 };
+// 
+
+class Dog {
+public:
+	Dog() {
+		// 다음시간에
+	}
+
+// Dog 만 마리를 생성하여 파일에 저장(write), 메모리로 읽어와서 처리(정렬)
+
+private:
+	std::string name;		// 32바이트
+	std::size_t id;			// 8바이트
+};
 
 int main()
 {
-	for (int i = 0; i < 1000; ++i) {
-		std::print("{:8}", uid(dre));
-	}
 
 	save("main.cpp");
 }
