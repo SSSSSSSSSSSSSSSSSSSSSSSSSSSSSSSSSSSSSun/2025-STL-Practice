217a9785e2927d5cc5bb3d6524305494151ba35c
SSun <90237785+SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSun@users.noreply.github.com>
2025-06-10 15:10:21 +0900

    6/10

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 //------------------------------------------------------------------
-// 2025 STL 화56목78	5월 29일 목요일							(13주 1일)
+// 2025 STL 화56목78	6월 10일 화요일							(14주 1일)
 // 6월 19일 목요일 15주 2일 - 기말 시험
 //------------------------------------------------------------------
 // VS 버전 - 17.13 이상
 // Release / x64, C++언어표준 - /std::c++latest, SDL 검사 - 아니오
 //------------------------------------------------------------------
-// Unordered Associative Containers - hash 구조
+// Algorithm - cppreference
 //------------------------------------------------------------------
 #include <iostream>
-#include <unordered_set>
-#include <print>
+#include <string>
+#include <algorithm>
 #include "save.h"
 #include "STRING.h"
 // using namespace std;					// 우리는 이렇게 하면 안된다
@@ -18,40 +18,15 @@
 
 extern bool watching;					// 관찰하려면 true로 설정
 
-template <>
-struct std::hash<STRING> {
-	size_t operator()(const STRING& s) const {
-		//std::string str(s.begin(), s.end());
-		return std::hash<std::string>{}(std::string(s.begin(), s.end()));
-	}
-};
-
 int main()
 {
+	std::string s{ "2025. 6. 10." };
 
-	std::unordered_set<STRING, std::hash<STRING>> us{"1", "22", "333", "4444", "55555"};
-
-	for (const STRING& s : us) {
+	while (1) {
 		std::cout << s << '\n';
+		// 100ms 쉬고
+		std::rotate(s.begin(), s.end(), s.begin() + 1);
 	}
 
-	// unordered_set의 메모리를 화면에 출력한다.
-
-	while (true) {
-		for (size_t bc = 0; bc < us.bucket_count(); ++bc) {
-			std::print("[{:>3}]", bc);
-			for (auto i = us.begin(bc); i != us.end(bc); ++i) {
-				std::print(" -> {:}", std::string{ i->begin(), i->end() });
-			}
-
-			std::cout << '\n';
-		}
-
-		std::cout << "추가할 STRING - ";
-		STRING s;
-		std::cin >> s;
-
-		us.insert(s);
-	}
 	save("main.cpp");
 }
